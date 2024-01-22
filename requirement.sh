c261488f988e017c29f5c6e169117499b1b4cb08
whoami0100 <155895047+whoami0100@users.noreply.github.com>
2024-01-22 14:31:47 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -3,5 +3,6 @@ echo "checking all requirements packages..........."
 echo "installing some packages and libraries......."
 pip install deep-translator
 pip install gTTS
+mkdir audio
 echo "all done............."
 
